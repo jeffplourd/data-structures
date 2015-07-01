397f157136a61011544e1ad183a90d1e7c986603
jeffplourd <jeffplourd@gmail.com>
2015-07-01 16:41:43 -0700

    finished second portion, shared functional instantiation.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,43 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newObj = {};
+  newObj.length = 0;
+  newObj.storage = {next:null};
+  _.extend(newObj, queueMethods);
+  return newObj;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+	this.length++;
+	var end = this.storage;
+	while(end.next !== null) {
+		end = end.next;
+	}
+	end.data = value;
+	end.next = {next:null};
+};
+
+queueMethods.dequeue = function() {
+	if(this.length>1) {
+		this.length--;
+		var temp = this.storage.data;
+		this.storage = this.storage.next;
+		return temp;
+	}else if(this.length === 1) {
+		this.length--;
+		var temp = this.storage.data;
+		this.storage = {next:null};
+		return temp;
+	}else {
+		return 0;
+	}
+};
+
+queueMethods.size = function() {
+	return this.length;
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+	var newObj = {};	
+	newObj.storage = {}; 
+	newObj.length = 0; 
+
+	_.extend(newObj, stackMethods);
+
+	return newObj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+	push: function(value){
+		this.length++; 
+		this.storage[this.length] = value; 
+	}, 
+	pop: function(){
+		if(this.length>=1){
+			this.length--;
+		}
+		
+		return this.storage[this.length+1];
+	}, 
+	size: function(){
+		return this.length; 
+	}
 
+};
 
