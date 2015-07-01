85acc7cc8543035804fcdd67e32151dc8a6b7fc5
jeffplourd <jeffplourd@gmail.com>
2015-07-01 15:47:52 -0700

    Completes functional instantiation for stack and queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,17 +2,48 @@ var Queue = function(){
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {next:null};
+  var length = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    length++;
+
+    var end = storage; 
+    while(end.next!==null){
+      end = end.next; 
+    }
+    //end is going to be the last one on the list
+    end.data = value;
+    end.next = {next:null}; 
+    
   };
 
   someInstance.dequeue = function(){
+    if(length>1){
+      
+      length--;
+      var result = storage.data; 
+      storage = storage.next; 
+      
+      return result;
+    }else if(length ==1){
+      length--;
+      var result = storage.data; 
+      storage = {next:null}; 
+      
+      return result; 
+
+    }else{
+      return 0; 
+    }
+
+
   };
 
   someInstance.size = function(){
+    return length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length=0; 
 
   // Implement the methods below
   someInstance.push = function(value){
+    length++; 
+    storage[length] = value; 
   };
 
   someInstance.pop = function(){
+    if(length>=1){
+      length--; 
+      return storage[length+1]; 
+    }else{
+      return 0; 
+    }
   };
 
   someInstance.size = function(){
+    return length; 
   };
 
   return someInstance;
